cc271fa25f225fd90e83a24640b3f7d86f0019ec
Marwan <marwan69120@live.fr>
2017-10-20 23:54:13 +0200

    Add files via upload

---
+++ DCP-Core/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using static System.Drawing.Color;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace DCP_Core
+{
+    public partial class Form1 : Form
+    {
+        private static RegistryKey DCPre = Registry.CurrentUser.OpenSubKey("Software\\Despair Codec Pack");
+        private String DCPVersion = DCPre.GetValue("Version").ToString();
+        private String DCPPath = DCPre.GetValue("InstallPath").ToString();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rundll32(String relativePath, String dll, String args)
+        {
+            String path = DCPPath + relativePath;
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            //startInfo.Verb = "RunAs";
+            startInfo.FileName = "rundll32";
+            startInfo.Arguments = "\"" + path + dll + "\"" + "," + args;
+            var process = Process.Start(startInfo);
+            process.WaitForExit();
+        }
+
+        private void runPrgm(String relativePath, String exe, String args="", bool waitForExit=true)
+        {
+            String path = DCPPath + relativePath;
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            //startInfo.Verb = "RunAs";
+            startInfo.FileName = DCPPath + relativePath + exe;
+            startInfo.Arguments = args;
+            var process = Process.Start(startInfo);
+            if (waitForExit) process.WaitForExit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // LAV Splitter
+            String path = "\\LAVFilters\\";
+            String file = "LAVSplitter.ax";
+            String args = "OpenConfiguration";
+            rundll32(path, file, args);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // LAV Video
+            String path = "\\LAVFilters\\";
+            String file = "LAVVideo.ax";
+            String args = "OpenConfiguration";
+            rundll32(path, file, args);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // LAV Audio
+            String path = "\\LAVFilters\\";
+            String file = "LAVAudio.ax";
+            String args = "OpenConfiguration";
+            rundll32(path, file, args);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // XySubFilter
+            String path = "\\XySubFilter\\";
+            String file = "XySubFilter.dll";
+            String args = "XySubFilterConfiguration";
+            rundll32(path, file, args);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://despair-paradise.com/dcp");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            // Associate Files
+            String path = "\\MPC-BE\\";
+            String file = "mpc-be64.exe";
+            String args = "/adminoption 10027";
+            runPrgm(path, file, args);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // MadVR
+            String path = "\\madVR\\";
+            String file = "madHcCtrl.exe";
+            String args = "";
+            runPrgm(path, file, args, false);
+            MessageBox.Show("Right click on MadVR icon in System Tray");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            // Presets
+            String path = "\\Config\\";
+            String file = "DCP-Settings.exe";
+            runPrgm(path, file);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            // Reinstall
+            String path = "\\";
+            String file = "install.bat";
+            runPrgm(path, file);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Check for Update
+            string url = "http://files.automne.me/dcp/lastVersion";
+            string contents;
+            using (var wc = new System.Net.WebClient())
+                contents = wc.DownloadString(url);
+            contents = contents.Remove(contents.Length - 1);
+            if (contents == DCPVersion) MessageBox.Show("You are using the latest version !\nNo update required.");
+            else
+            {
+                MessageBox.Show("A new release is available.               \nCurrent Version :   " + DCPVersion + "\nNew Version :        " +
+                                contents);
+                System.Diagnostics.Process.Start("http://despair-paradise.com/dcp");
+
+            }
+        }
+
+
+
+        private void PaintBorderlessGroupBox(object sender, PaintEventArgs p)
+        {
+            GroupBox box = (GroupBox)sender;
+            p.Graphics.Clear(SystemColors.Control);
+            var brush = new SolidBrush(Color.FromArgb(255, 41, 47, 51));
+            p.Graphics.DrawString(box.Text, box.Font, brush, 0, 0);
+        }
+
+    }
+}
---
+++ DCP-Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("DCP Core Panel")]
+[assembly: AssemblyDescription("Despair Codec Pack Manager")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Despair-Paradise")]
+[assembly: AssemblyProduct("DCP-Core")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("5962e5e1-cb79-47a5-bad0-e7a123054d65")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.8")]
+[assembly: AssemblyFileVersion("1.1.8")]
